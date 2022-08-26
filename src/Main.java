1e4f85e6b015cd87864f1f0e5ef479a5ca7e59a2
AlexanderRyaz <110193564+AlexanderRyaz@users.noreply.github.com>
2022-08-26 12:38:32 +0300

    task 2

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,26 @@
 import java.time.LocalDate;
 
 public class Main {
-
-    public static void currentYear() {
-        int currentYear = LocalDate.now().getYear();
-        if (currentYear % 4 != 0 && currentYear % 400 != 0) {
-            System.out.println(" Не является высокосным");
-        } else {
-            System.out.println("Является высокосным");
+    public static void versionOs() {
+        int clientOS = 0;
+        int clientDeviceYear = LocalDate.now().getYear();
+        if (clientOS == 0) {
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для для iOS по ссылке");
+            }
+            if (clientDeviceYear < 2015) {
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            } else {
+                System.out.println("Установите версию приложения для для Android по ссылке");
+            }
         }
+
     }
     public static void main(String[] args) {
-        currentYear();
+        versionOs();
     }
 }
-
-
 
 
