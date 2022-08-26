38fcd8d8336f63d26c1b1f0b3e1c331ead8dcb88
AlexanderRyaz <110193564+AlexanderRyaz@users.noreply.github.com>
2022-08-26 12:05:06 +0300

    task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.time.LocalDate;
+
 public class Main {
+
+    public static void currentYear() {
+        int currentYear = LocalDate.now().getYear();
+        if (currentYear % 4 != 0 && currentYear % 400 != 0) {
+            System.out.println(" Не является высокосным");
+        } else {
+            System.out.println("Является высокосным");
+        }
+    }
     public static void main(String[] args) {
-        System.out.println("go");
+        currentYear();
     }
 }
+
+
+
+
